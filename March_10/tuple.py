4cd58836a634d204634148a93f9947c488be2dd1
ZhouZhuoFei <48236134+ZhouZhuofei@users.noreply.github.com>
2019-03-10 21:52:53 +0800

    Create tuple.py

---
+++ March_10/tuple.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+t1 = 1, 2, 3
+t2 = 'hello', 'guy'
+t3 =(3,)
+#tuple function
+t4 = tuple(['hello', 'world'])
+t5 = tuple('hello')
+print(t5)
+#('h', 'e', 'l', 'l', 'o')
+MAX = ('hello', 'world', 2015, 2016)
+print("MAX[1] is:", MAX[1])
+print("MAX[0] is :", MAX[0])
+
+#############
+field = ('hello', 'world')
+num = (2015, 2016)
+print("add:",field + num)#add: ('hello', 'world', 2015, 2016)
+#delect
+del field
+
+
+
+
